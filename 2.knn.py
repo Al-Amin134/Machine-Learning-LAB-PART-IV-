f0c93ea3dd462b32090fd800752e68d1c2d8784f
Md.Al-Amin Babu <mdalaminbabu.210@gmail.com>
2026-08-15 21:50:36 +0600

    2.knn.py

--- 2.knn.py
+++ 2.knn.py
@@ -9,7 +9,6 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix, accuracy_score, classification_report, f1_score, precision_score, recall_score
 from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn.decomposition import PCA
-#help(plt.plot)
 ##################### 1.load data ############################
 df = pd.read_csv("Dry_Bean.csv")
 print(df.head())
